a13b463bbad32f45d2a26cb1175c56ba866d55a0
muslihaashraf <muslihaashraf@gmail.com>
2026-09-10 11:14:32 +0530

    Update sumarray.c

--- musliha c/sumarray.c
+++ musliha c/sumarray.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
-  2 int SumOfArray(int arr[],int n )
-  3   {
-  4     int sum=0;
-  5     for(int i=0;i<n;i++)
-  6     {
-  7
-  8         sum+=arr[i];
-  9     }
- 10     return sum;
- 11  }
- 12 int main()
- 13 {
- 14     int n;
- 15     printf("enter number of array elements:");
- 16     scanf("%d",&n);
- 17     int arr[n];
- 18     printf("enter %d  elements:\n",n);
- 19     for(int i=0;i<n;i++){
- 20     scanf("%d",&arr[i]);
- 21     }
- 22     int result=SumOfArray(arr,n);
- 23     printf("sum = %d\n",result);
+ int SumOfArray(int arr[],int n )
+   {
+     int sum=0;
+     for(int i=0;i<n;i++)
+     {
+         sum+=arr[i];
+     }
+     return sum;
+   }
+ int main()
+  {
+     int n;
+     printf("enter number of array elements:");
+     scanf("%d",&n);
+     int arr[n];
+     printf("enter %d  elements:\n",n);
+     for(int i=0;i<n;i++){
+      scanf("%d",&arr[i]);
+      }
+     int result=SumOfArray(arr,n);
+     printf("sum = %d\n",result);
      }
    
 
