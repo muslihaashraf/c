3688a75849af9789d2fb7bffc9b8a398654f789e
muslihaashraf <muslihaashraf@gmail.com>
2026-09-10 11:12:00 +0530

    Update largest.c

--- musliha c/largest.c
+++ musliha c/largest.c
@@ -1,25 +1,24 @@
  #include<stdio.h>
-  2 int largest(int arr[],int n){
-  3     int max=arr[0];
-  4     for(int i=0;i<n;i++){
-  5         if(arr[i]>max){
-  6             max=arr[i];
-  7         }
-  8     }
-  9 return max;
- 10 }
- 11 int main(){
- 12     int arr[100],n;
- 13     printf("enter number of elements:");
- 14     scanf("%d",&n);
- 15     printf("enter %d elements:\n",n);
- 16     for(int i=0;i<n;i++){
- 17     scanf("%d",&arr[i]);
- 18     }
- 19     printf("largest=%d\n",largest(arr,n));
- 20
- 21 return 0;
- 22 }
+ int largest(int arr[],int n){
+      int max=arr[0];
+      for(int i=0;i<n;i++){
+           if(arr[i]>max){
+               max=arr[i];
+           }
+       }
+  return max;
+ }
+ int main(){
+     int arr[100],n;
+     printf("enter number of elements:");
+     scanf("%d",&n);
+     printf("enter %d elements:\n",n);
+     for(int i=0;i<n;i++){
+     scanf("%d",&arr[i]);
+     }
+     printf("largest=%d\n",largest(arr,n));
+  return 0;
+ }
 
 
 output:-
